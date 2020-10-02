e604e5fda5169508253972ff628a5872159a6ba4
LON DUFF <johncduffman@gmail.com>
2020-10-02 13:20:16 -0400

    added poem

--- poetry-slam.py
+++ poetry-slam.py
@@ -10,18 +10,18 @@ print(get_file_lines("poem.txt"))
 
 def lines_printed_backwards(lines_list): 
     lines_list.reverse()
-    lines_length = len (lines_list)
-    for i in range (lines_length): 
+    lines_length = len(lines_list)
+    for i in range(lines_length): 
         line = lines_list[i]
-        line_num = lines_length - i 
-        print(f"{line_num}{line}")
+        line_num = lines_length - i
+        print(f"{line_num} {line}")
 
 # The above function will print the lines of the poem.txt file in 
 # backwards order
 
 def lines_printed_random(lines_list):
-    for lines in lines_list:
-        print(choice(lines_list))
+     for lines in lines_list:
+         print(choice(lines_list))
 
 # The lines_printed_random function creates a random-ordered print
 # out of the same lines list 
@@ -35,8 +35,8 @@ def lines_printed_custom(lines_list):
 
 # The above function prints every tenth line of the poem.txt file 
 
-lines_list = get_file_lines('poem.txt')
-poem_lines = get_file_lines('poem.txt')
+lines_list = get_file_lines("poem.txt")
+poem_lines = get_file_lines("poem.txt")
 
 lines_printed_backwards(lines_list)
 lines_printed_custom(lines_list)
